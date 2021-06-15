e684b57e38c167ba82dbc8b4a585adfec9a4ca20
Adriano Castro <eaadrianosilvacastro@unipam.edu.br>
2021-06-14 23:36:39 -0300

    First Commit, Editora, Genero

---
+++ src/main/java/br/edu/unipam/entity/Editora.java
@@ -0,0 +1,22 @@
+package br.edu.unipam.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Entity
+public class Editora extends Livro implements Serializable{
+  @NotNull  
+  @Size(min = 5, max = 200)
+  @Column(name = "NomeEditora")
+  private String NomeEditora;
+}
